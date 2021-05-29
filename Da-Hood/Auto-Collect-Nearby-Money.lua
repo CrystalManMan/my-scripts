8727d261d80c050554565b8eba3c94c160f322b8
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-29 22:47:33 +0100

    Update Auto-Collect-Nearby-Money.lua

--- Da-Hood/Auto-Collect-Nearby-Money.lua
+++ Da-Hood/Auto-Collect-Nearby-Money.lua
@@ -1,4 +1,4 @@
-function FindMoneyNearBy()
+function FindMoneyNearby()
     for i,v in pairs(game:GetService("Workspace").Ignored.Drop:GetChildren()) do
         if v.Name == "MoneyDrop" then
             if (game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position - v.Position).Magnitude <= 12 then -- Dont put the magnitude above 12
@@ -10,5 +10,5 @@ function FindMoneyNearBy()
 end
 
 game:GetService("RunService").Stepped:Connect(function()
-    FindMoneyNearBy()
+    FindMoneyNearby()
 end)
