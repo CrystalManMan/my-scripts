9bcdf1b971e177359b31b68c3dc523051fd9043a
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-29 20:57:45 +0100

    Update Auto-Drop-Money.lua

--- Da-Hood/Auto-Drop-Money.lua
+++ Da-Hood/Auto-Drop-Money.lua
@@ -1,11 +1,11 @@
-getgenv().MoneyDrop = true -- execute getgenv().CashDrop = false to turn off the cash drop
-getgenv().Money = 100 -- put this above 100 or lower than 10000
+getgenv().MoneyDrop = true -- Execute getgenv().CashDrop = false to turn off the cash drop
+getgenv().Money = 100 -- Put this above 100 or lower than 10000
 
 function DropMoney(Amount)
     game:GetService("ReplicatedStorage").MainEvent:FireServer("DropMoney", Amount)
 end
 
 while MoneyDrop do
-    DropMoney(Money) -- loop drops the amount of cash you set
-    wait(15) -- im not sure what the delay on this is, change if you know what the delay is
+    DropMoney(Money) -- Loop drops the amount of cash you set
+    wait(15) -- Im not sure what the delay on this is, change if you know what the delay is
 end
