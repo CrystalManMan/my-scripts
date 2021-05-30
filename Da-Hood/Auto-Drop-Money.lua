66380d033a8e335f5a59cb7e591b6921f4a4d913
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-30 12:00:46 +0100

    Update Auto-Drop-Money.lua

--- Da-Hood/Auto-Drop-Money.lua
+++ Da-Hood/Auto-Drop-Money.lua
@@ -1,6 +1,17 @@
 getgenv().MoneyDrop = true -- Execute getgenv().MoneyDrop = false to turn off the cash drop
 getgenv().Money = 100 -- Put this above 100 or lower than 10000
 
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Auto Drop Money by zefify/REVlENGE")
+
 function DropMoney(Amount)
     game:GetService("ReplicatedStorage").MainEvent:FireServer("DropMoney", Amount)
 end
