3427311e6ca77cb1cadd081789f15c131e337ada
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-30 12:37:39 +0100

    Update Auto-Drop-Money.lua

--- Da-Hood/Auto-Drop-Money.lua
+++ Da-Hood/Auto-Drop-Money.lua
@@ -18,7 +18,7 @@ function DropMoney(Amount)
     end
 end
 
-while MoneyDrop do
+while getgenv().MoneyDrop do
     DropMoney(Money) -- Loop drops the amount of cash you set
     wait(15) -- Im not sure what the delay on this is, change this if you know what the delay is
 end
