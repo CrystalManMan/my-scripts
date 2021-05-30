ef11a10146b09e92e635e6ebf33b996e3625cc52
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-30 23:07:15 +0100

    Update Auto-Hide-Name.lua

--- Da-Hood/Auto-Hide-Name.lua
+++ Da-Hood/Auto-Hide-Name.lua
@@ -3,15 +3,16 @@ getgenv().CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRoot
 
 game:GetService("RunService").Stepped:Connect(function()
     if getgenv().HideName == true then
-        if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("In-gameMask") and not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mask") then
+        if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("In-gameMask") and not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mask") or game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mask") then
+            wait(0.05)
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Riot Mask] - $60"].Head.CFrame
-            wait(0.25)
+            wait(0.05)
             fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Riot Mask] - $60"].ClickDetector)
-            wait(0.25)
+            wait(0.05)
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame
-            wait()
+            wait(0.05)
             game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mask").Parent = game:GetService("Players").LocalPlayer.Character
-            wait()
+            wait(0.05)
             game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mask"):Activate()
         end
     end
