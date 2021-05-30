87228305d64398bc55d6a26292f75e01d7158240
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-30 12:47:22 +0100

    Update Auto-Unban.lua

--- Da-Hood/Auto-Unban.lua
+++ Da-Hood/Auto-Unban.lua
@@ -23,22 +23,21 @@ Character:WaitForChild("BodyEffects").Dead:Destroy()
 local Value = Instance.new("BoolValue", Character.BodyEffects)
 Value.Name = "Dead"
 
-game:GetService("Players").LocalPlayer.Character.Humanoid.Died:Connect(function()
-    if getgenv().AutoUnban == true then
-        wait(4.9)
-        game:GetService("Players").LocalPlayer.Character:ClearAllChildren()
-        local Character = game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
-        local Folder = Instance.new("Folder")
-        Folder.Name = "FULLY_LOADED_CHAR"
-        Folder.Parent = Character
-        Character:WaitForChild("BodyEffects").Dead:Destroy()
-        local Value = Instance.new("BoolValue", Character.BodyEffects)
-        Value.Name = "Dead"
-    end
-end)
-
 game:GetService("RunService").Stepped:Connect(function()
     if game:GetService("Players").LocalPlayer.Character.BodyEffects:FindFirstChild("K.O").Value == true then
         game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(15)
     end
+    game:GetService("Players").LocalPlayer.Character.Humanoid.Died:Connect(function()
+        if getgenv().AutoUnban == true then
+            wait(4.9)
+            game:GetService("Players").LocalPlayer.Character:ClearAllChildren()
+            local Character = game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
+            local Folder = Instance.new("Folder")
+            Folder.Name = "FULLY_LOADED_CHAR"
+            Folder.Parent = Character
+            Character:WaitForChild("BodyEffects").Dead:Destroy()
+            local Value = Instance.new("BoolValue", Character.BodyEffects)
+            Value.Name = "Dead"
+        end
+    end)
 end)
