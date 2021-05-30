c258da7babc3d9ab93e76f75a87d0423baaaebbc
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-30 23:10:34 +0100

    Update Auto-Hide-Name.lua

--- Da-Hood/Auto-Hide-Name.lua
+++ Da-Hood/Auto-Hide-Name.lua
@@ -5,9 +5,9 @@ game:GetService("RunService").Stepped:Connect(function()
     if getgenv().HideName == true then
         if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("In-gameMask") and not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mask") or game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mask") then
             wait(0.05)
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Riot Mask] - $60"].Head.CFrame
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Paintball Mask] - $60"].Head.CFrame
             wait(0.05)
-            fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Riot Mask] - $60"].ClickDetector)
+            fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Paintball Mask] - $60"].ClickDetector)
             wait(0.05)
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame
             wait(0.05)
