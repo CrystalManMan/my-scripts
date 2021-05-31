32229a54c8a943d210730915c915d92366a86184
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-05-31 13:43:35 +0100

    Update Anti-Flashbang.lua

--- Da-Hood/Anti-Flashbang.lua
+++ Da-Hood/Anti-Flashbang.lua
@@ -1,5 +1,16 @@
 getgenv().AntiFlashbang = true -- Execute getgenv().AntiFlashbang = false to turn off anti flashbang
 
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Anti Flashbang by zefify/REVlENGE")
+
 function RFB()
     if game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui:FindFirstChild("whiteScreen") then
         game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui:FindFirstChild("whiteScreen"):Destroy()
