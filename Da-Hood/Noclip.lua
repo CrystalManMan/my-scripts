cad1aa4da762633c5e12a2d22a6d362556a86dea
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-05 19:30:42 +0100

    Create Noclip.lua

---
+++ Da-Hood/Noclip.lua
@@ -0,0 +1,21 @@
+getgenv().Noclip = true -- Execute getgenv().Noclip = false to turn off noclip
+
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Noclip by zefify/REVlENGE")
+
+game:GetService("RunService").Stepped:Connect(function()
+    if getgenv().Noclip == true and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid") then
+        pcall(function()
+            game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("BasePart").CanCollide = false
+            game:GetService("Players").LocalPlayer.Character.Humanoid.Sit = false
+        end)
+    end
+end)
