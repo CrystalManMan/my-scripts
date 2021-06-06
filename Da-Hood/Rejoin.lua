949502c62aad0b7ee6df895898a1a8a3b290c328
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-06 17:01:39 +0100

    Update Rejoin.lua

--- Da-Hood/Rejoin.lua
+++ Da-Hood/Rejoin.lua
@@ -1,2 +1,14 @@
 -- Made this cause some of you Da Hood scripters dont know how to make a simple rejoin script and instead make a serverhop script :joy:
+
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Rejoin by zefify/REVlENGE")
+wait(1)
 game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
