790f72e80b71f3ca60c48fb8b7069ee191ec20c3
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-06 16:59:09 +0100

    Update Rejoin.lua

--- Da-Hood/Rejoin.lua
+++ Da-Hood/Rejoin.lua
@@ -1,2 +1,2 @@
--- Made this cause some of you Da Hood scripters dont know how to make a simple rejoin script and instead make a serverhop script :rofl:
+-- Made this cause some of you Da Hood scripters dont know how to make a simple rejoin script and instead make a serverhop script :joy:
 game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
