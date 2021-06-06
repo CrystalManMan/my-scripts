853c20f026b1685d09e43b094b30d0040766d27e
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-06 16:39:52 +0100

    Create Spam-Call.lua

---
+++ Da-Hood/Spam-Call.lua
@@ -0,0 +1,14 @@
+getgenv().SpamPhoneCall = true -- Execute getgenv().SpamPhoneCall = false to turn off spam phone call
+getgenv().SpamCallTarget = "Player123" -- Change this to the name of the player you want to spam call (Make sure to add the capital letters)
+
+function SPC()
+    if game:GetService("Players").LocalPlayer.Character:FindFirstChild("[Phone]") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("[Phone]") then
+        game:GetService("ReplicatedStorage").MainEvent:FireServer("PhoneCall", CallTarget)
+    end
+end
+
+game:GetService("RunService").Stepped:Connect(function()
+    if getgenv().SpamPhoneCall == true and getgenv().SpamCallTarget ~= nil then
+        SPC()
+    end
+end)
