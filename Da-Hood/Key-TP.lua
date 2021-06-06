4a1d0a4c331cc21e10bde288e7097fbbe866d96a
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-06 21:05:30 +0100

    Update Key-TP.lua

--- Da-Hood/Key-TP.lua
+++ Da-Hood/Key-TP.lua
@@ -2,7 +2,7 @@ getgenv().TP = true -- Execute getgenv().TP = false to turn off click tp
 getgenv().Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 
 Mouse.KeyDown:Connect(function(Key)
-    if Key == "t" then -- 
+    if Key == "t" then -- Change this to whatever key you want
         if getgenv().TP == true then
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Mouse.hit.p.X, Mouse.hit.p.Y, Mouse.hit.p.Z)
         end
