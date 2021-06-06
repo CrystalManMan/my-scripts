1c095d80b6f66360644008cc0ec898570748f433
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-06 17:01:59 +0100

    Update Spam-Call.lua

--- Da-Hood/Spam-Call.lua
+++ Da-Hood/Spam-Call.lua
@@ -1,6 +1,17 @@
 getgenv().SpamPhoneCall = true -- Execute getgenv().SpamPhoneCall = false to turn off spam phone call
 getgenv().SpamCallTarget = "Player123" -- Change this to the name of the player you want to spam call (Make sure to add the capital letters)
 
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Spam Phone Call by zefify/REVlENGE")
+
 function SPC()
     if game:GetService("Players").LocalPlayer.Character:FindFirstChild("[Phone]") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("[Phone]") then
         game:GetService("ReplicatedStorage").MainEvent:FireServer("PhoneCall", getgenv().SpamCallTarget)
