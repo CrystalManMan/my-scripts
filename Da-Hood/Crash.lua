3bcb81c30426f08b923b67c4d3d92d55fdd5f6fa
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-07 13:53:46 +0100

    Update Crash.lua

--- Da-Hood/Crash.lua
+++ Da-Hood/Crash.lua
@@ -1,7 +1,21 @@
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LEWISLEWISLEWISLEWISLEWIS/my-scripts/main/Da-Hood/Auto-Unban.lua"))()
+end)
+
+wait(7.5)
+
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LEWISLEWISLEWISLEWISLEWIS/my-scripts/main/Da-Hood/Godblock.lua"))()
+end)
+
 Money = game:GetService("Players").LocalPlayer.DataFolder.Currency.Value
 Tool = Instance.new("Tool", game:GetService("Players").LocalPlayer.Backpack)
 Tool.Name = "Crash"
 Tool.ToolTip = "Hold this tool to buy tasers"
+
+wait(0.5)
+
+game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Crash").Parent = game:GetService("Players").LocalPlayer.Character
 
 game:GetService("RunService").Stepped:Connect(function()
     if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Crash") then
@@ -12,6 +26,8 @@ game:GetService("RunService").Stepped:Connect(function()
     end
     if HoldingCrash == true then
         if Money > 100000 then -- You're probably gonna need atleast 100k
+            game:GetService("ReplicatedStorage").MainEvent:FireServer("Block", true)
+            wait()
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Taser] - $1000"].Head.CFrame
             wait()
             game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(11)
