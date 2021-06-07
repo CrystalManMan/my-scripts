6d5c42acfaad964aa37a06926db03e7bc7e72453
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-07 15:03:54 +0100

    Update Key-TP.lua

--- Da-Hood/Key-TP.lua
+++ Da-Hood/Key-TP.lua
@@ -1,6 +1,17 @@
 getgenv().TP = true -- Execute getgenv().TP = false to turn off click tp
 getgenv().Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 
+function Notification(text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Da Hood",
+        Text = text,
+        Icon = "rbxassetid://0",
+        Duration = 3,
+    })
+end
+
+Notification("Key Tp by zefify/REVlENGE")
+
 Mouse.KeyDown:Connect(function(Key)
     if Key == "t" then -- Change this to whatever key you want
         if getgenv().TP == true then
