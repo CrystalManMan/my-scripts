6f6c8230c5eb16483912b2233ca98221dd31dd01
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-15 22:07:26 +0100

    Update Godblock.lua

--- Da-Hood/Godblock.lua
+++ Da-Hood/Godblock.lua
@@ -7,14 +7,13 @@ function Notification(text)
     })
 end
 
-Notification("Godblock by zefify/REVlENGE")
-
 function Godblock()
     if game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Size.X == 2 and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Size.Y == 2 then -- Unban size / 0 lettuce eaten size
         game:GetService("Players").LocalPlayer.Character.BodyEffects:FindFirstChild("Defense"):Destroy()
         local Val = Instance.new("IntValue", game:GetService("Players").LocalPlayer.Character.BodyEffects)
         Val.Name = "Defense"
         Val.Value = 100
+        Notification("Godblock by zefify/REVlENGE")
         Notification("God block is now on, Reset to turn god block off")
     end
 end
