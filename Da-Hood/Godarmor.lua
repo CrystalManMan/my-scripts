41a9fdcaf2b370d872f4be07d91524dce0ee08ad
LEWISLEWISLEWISLEWISLEWIS <81487451+LEWISLEWISLEWISLEWISLEWIS@users.noreply.github.com>
2021-06-27 14:36:38 +0100

    Update Godarmor.lua

--- Da-Hood/Godarmor.lua
+++ Da-Hood/Godarmor.lua
@@ -7,14 +7,13 @@ function Notification(text)
     })
 end
 
-Notification("Godarmor by zefify/REVlENGE")
-
 function Godarmor()
     if game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Size.X == 2 and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Size.Y == 2 then -- Unban size / 0 lettuce eaten size
         game:GetService("Players").LocalPlayer.Character.BodyEffects:FindFirstChild("Armor"):Destroy()
         local Val = Instance.new("IntValue", game:GetService("Players").LocalPlayer.Character.BodyEffects)
         Val.Name = "Armor"
         Val.Value = 100
+        Notification("Godarmor by zefify/REVlENGE")
         Notification("God armor is now on, Reset to turn god armor off")
     end
 end
